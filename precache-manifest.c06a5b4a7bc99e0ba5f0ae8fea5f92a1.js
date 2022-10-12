9b945f5531f000250131b9fa2c85de0c76010d81
AC-Oliveira <allancardoso21@gmail.com>
2022-10-12 10:38:11 -0300

    Updates

---
+++ precache-manifest.c06a5b4a7bc99e0ba5f0ae8fea5f92a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608658317d3744f0eb63f062ebf53969",
+    "url": "/Trybe-Projects/2-front-end/01-movie-library-cards/index.html"
+  },
+  {
+    "revision": "dd35a8487f05632f6263",
+    "url": "/Trybe-Projects/2-front-end/01-movie-library-cards/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/Trybe-Projects/2-front-end/01-movie-library-cards/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "dd35a8487f05632f6263",
+    "url": "/Trybe-Projects/2-front-end/01-movie-library-cards/static/js/main.f0c0c0a5.chunk.js"
+  },
+  {
+    "revision": "ae73c02253db922d192f",
+    "url": "/Trybe-Projects/2-front-end/01-movie-library-cards/static/js/runtime-main.d8d03313.js"
+  }
+]);
