ffbda9de901fee84c3e1f8fd061fe18a5f793171
AC-Oliveira <allancardoso21@gmail.com>
2022-10-12 12:34:23 -0300

    adding frontend project

---
+++ docs/2-front-end/02-movie-cards-library-stateful/precache-manifest.58dc9b0799fbf34c4aabd3b79f755b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c6028bef0eb610ee31880a2fc347c3",
+    "url": "/Trybe-Projects/2-front-end/02-movie-cards-library-stateful/index.html"
+  },
+  {
+    "revision": "ea308cdb9418b69cb279",
+    "url": "/Trybe-Projects/2-front-end/02-movie-cards-library-stateful/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "97a4727aab243b59fa66",
+    "url": "/Trybe-Projects/2-front-end/02-movie-cards-library-stateful/static/js/2.0d072f2a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Trybe-Projects/2-front-end/02-movie-cards-library-stateful/static/js/2.0d072f2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea308cdb9418b69cb279",
+    "url": "/Trybe-Projects/2-front-end/02-movie-cards-library-stateful/static/js/main.4b1057c3.chunk.js"
+  },
+  {
+    "revision": "12f5e08fcac546b67ea0",
+    "url": "/Trybe-Projects/2-front-end/02-movie-cards-library-stateful/static/js/runtime-main.dd036a3e.js"
+  }
+]);
---
+++ docs/2-front-end/05-react-testing-library/precache-manifest.7a2c97ed410a5f729ba8191b3382ed7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3680648017ef8ea4b68dc663fe85f51",
+    "url": "/Trybe-Projects/2-front-end/05-react-testing-library/index.html"
+  },
+  {
+    "revision": "66a62198e4c6a2c76e1e",
+    "url": "/Trybe-Projects/2-front-end/05-react-testing-library/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "8779f7fe88944579312b",
+    "url": "/Trybe-Projects/2-front-end/05-react-testing-library/static/js/2.585a919e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Trybe-Projects/2-front-end/05-react-testing-library/static/js/2.585a919e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a62198e4c6a2c76e1e",
+    "url": "/Trybe-Projects/2-front-end/05-react-testing-library/static/js/main.3a455d35.chunk.js"
+  },
+  {
+    "revision": "17bdbd31441e5a43c18e",
+    "url": "/Trybe-Projects/2-front-end/05-react-testing-library/static/js/runtime-main.1a0b886b.js"
+  }
+]);
