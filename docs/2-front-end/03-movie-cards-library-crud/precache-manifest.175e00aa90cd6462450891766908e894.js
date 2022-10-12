5ce9baac55b40895b2af20df3608115859440cf9
AC-Oliveira <allancardoso21@gmail.com>
2022-10-12 11:32:07 -0300

    adding project test

---
+++ docs/2-front-end/03-movie-cards-library-crud/precache-manifest.175e00aa90cd6462450891766908e894.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ae80727a3c93617eedd6ee96953f0a",
+    "url": "/2-front-end/03-movie-cards-library-crud/index.html"
+  },
+  {
+    "revision": "9b32e7c5180095b18d5b",
+    "url": "/2-front-end/03-movie-cards-library-crud/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5f591656f2881ff346b0",
+    "url": "/2-front-end/03-movie-cards-library-crud/static/js/2.1e39340d.chunk.js"
+  },
+  {
+    "revision": "9b32e7c5180095b18d5b",
+    "url": "/2-front-end/03-movie-cards-library-crud/static/js/main.1cd7874b.chunk.js"
+  },
+  {
+    "revision": "f1b9299498c537783a80",
+    "url": "/2-front-end/03-movie-cards-library-crud/static/js/runtime-main.6e35ad12.js"
+  }
+]);
