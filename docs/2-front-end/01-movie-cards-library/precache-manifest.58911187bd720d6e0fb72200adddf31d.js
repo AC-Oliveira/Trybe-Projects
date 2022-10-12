7e7365fc409c11731ce2c15a881a4163fddbd2ad
AC-Oliveira <allancardoso21@gmail.com>
2022-10-12 12:19:16 -0300

    adding frontend project

---
+++ docs/2-front-end/01-movie-cards-library/precache-manifest.58911187bd720d6e0fb72200adddf31d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e565e673c97ced549dca9f5938866e3",
+    "url": "/Trybe-Projects/2-front-end/01-movie-cards-library/index.html"
+  },
+  {
+    "revision": "1f049dca87f3d44c59a2",
+    "url": "/Trybe-Projects/2-front-end/01-movie-cards-library/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/Trybe-Projects/2-front-end/01-movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "1f049dca87f3d44c59a2",
+    "url": "/Trybe-Projects/2-front-end/01-movie-cards-library/static/js/main.c43f98d9.chunk.js"
+  },
+  {
+    "revision": "458e16c056f709ced9e4",
+    "url": "/Trybe-Projects/2-front-end/01-movie-cards-library/static/js/runtime-main.673b48dd.js"
+  }
+]);
